0d37a3da0c1896bf18f97dde8e6f89810f1a8093
Thomas Potherat <t.potherat@laposte.net>
2026-02-21 19:16:55 +0100

    feat: add lucide-angular integration and enhance collection component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,14 +2,17 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
 import { VocabularyEntryForm } from './vocabulary-entry-form/vocabulary-entry-form';
+import { Collection } from './collection/collection';
+import { LucideAngularModule } from 'lucide-angular';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, VocabularyEntryForm],
+  imports: [ LucideAngularModule, Header, VocabularyEntryForm, Collection],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('linguobuddy');
+  protected readonly title = signal('lingobuddy');
 
 }
--- src/app/collection/collection.ts
+++ src/app/collection/collection.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule, GraduationCap } from 'lucide-angular';
 
 @Component({
   selector: 'app-collection',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './collection.html',
   styleUrl: './collection.scss',
 })
-export class Collection {
-
-}
+export class Collection {}
