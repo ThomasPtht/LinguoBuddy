90e93110b5bc3b4e892f75142cf16c9dd232e50d
Thomas Potherat <t.potherat@laposte.net>
2026-03-23 09:39:08 +0100

    feat: enhance collection component with ChangeDetectorRef and NgZone for improved state management

--- src/app/collection/collection.ts
+++ src/app/collection/collection.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
-import { Router} from '@angular/router';
+import { Component, inject, OnInit, ChangeDetectorRef, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
 import { LucideAngularModule } from 'lucide-angular';
 import { VocabularyService } from '../services/vocabulary.service';
 import { VocabularyStats } from '../models/vocabulary.model';
@@ -12,27 +12,34 @@ import { VocabularyStats } from '../models/vocabulary.model';
   styleUrl: './collection.scss',
 })
 export class Collection implements OnInit {
-  // Injection des services
   private router = inject(Router);
   private vocabService = inject(VocabularyService);
+  private cdr = inject(ChangeDetectorRef);
+  private ngZone = inject(NgZone);
 
-  // Variable pour stocker les stats
   stats: VocabularyStats | null = null;
 
   ngOnInit() {
     this.loadStats();
   }
 
- loadStats() {
+  loadStats() {
     this.vocabService.getStats().subscribe({
       next: (data) => {
-        this.stats = data;
-        console.log('Stats récupérées :', data);
+        this.ngZone.run(() => {
+          this.stats = data;
+          this.cdr.detectChanges();
+        });
       },
-      error: (err) => console.error('Erreur lors du chargement des stats', err)
+      error: (err) => {
+        this.ngZone.run(() => {
+          console.error('erreur stats:', err);
+          this.cdr.detectChanges();
+        });
+      }
     });
   }
-  
+
   onFlashcards() {
     this.router.navigateByUrl('flashcards');
   }
